2f354ad9f163f01c7834ebd753dd36e8c17eed54
Dakskihedron <47210659+Dakskihedron@users.noreply.github.com>
2022-12-03 18:29:47 +1300

    Add day 3

---
+++ day-3.py
@@ -0,0 +1,25 @@
+with open('inputs/day-3.txt', 'r') as file:
+    data = file.read().strip().split('\n')
+    data = list(map(list, data))
+
+# Part one
+rugsacks = [[x[:len(x) // 2],x[len(x) // 2:]] for x in data]
+answer = 0
+for sack in rugsacks:
+    item = list(set(sack[0]).intersection(set(sack[1])))[0]
+    if item.isupper():
+        answer += ord(item) - 38
+    else:
+        answer += ord(item) - 96
+print(answer)
+
+# Part two
+groups = [data[i:i+3] for i in range(0, len(data), 3)]
+answer = 0
+for group in groups:
+    item = list((set(group[0]).intersection(set(group[1])).intersection(set(group[2]))))[0]
+    if item.isupper():
+        answer += ord(item) - 38
+    else:
+        answer += ord(item) - 96
+print(answer)
