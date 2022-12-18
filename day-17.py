8507e3edec80d5da0c640c1cc0092decc8c6e43e
Dakskihedron <47210659+Dakskihedron@users.noreply.github.com>
2022-12-18 23:12:10 +1300

    Cleanup day 17 part 1

--- day-17.py
+++ day-17.py
@@ -3,12 +3,8 @@ with open('inputs/day-17.txt', 'r') as file:
 
 
 # Part one
-def simulate_rockfall(data, world, units, logging=False):
-    fallen_count = 0
-    curr_height = 0
-
-    # Rock types
-    rock_types = {
+def simulate_rockfall(data, world, target):
+    rocks = {
         0: [(0,0), (1,0), (2,0), (3,0)],
         1: [(1,0), (0,1), (1,1), (2,1), (1,2)],
         2: [(0,0), (1,0), (2,0), (2,1), (2,2)],
@@ -16,93 +12,84 @@ def simulate_rockfall(data, world, units, logging=False):
         4: [(0,0), (1,0), (0,1), (1,1)]
     }
 
-    curr_rock_type = 0
-    curr_rock_pos = [p for p in rock_types[curr_rock_type]]
+    # Rocks fallen and height
+    fallen = 0
+    height = 0
 
+    # Current rock type and positions
+    rock_type = 0
+    rock_pos = [_ for _ in rocks[rock_type]]
+
+    # Initial rock position
+    rock_pos = [(x+2,y+3) for x,y in rock_pos]
+
+    # Current instruction
     ir = 0
 
-    # Init starting rock
-    for pos in range(len(curr_rock_pos)):
-        x, y = curr_rock_pos[pos]
-        x += 2
-        y += 3
-        curr_rock_pos[pos] = (x,y)
 
-    cycles = {}
+    # Main loop
+    while fallen != target:
 
-    while fallen_count != units:
-        if ir == 0:
-            if curr_rock_type in cycles:
-                cycles[curr_rock_type] += [(fallen_count, curr_height)]
-            else:
-                cycles[curr_rock_type] = [(fallen_count, curr_height)]
-
-            # Print last 50 positions and current rock shape positions
-            if logging:
-                keys = [k for k in list(world.keys())[-50:]]
-                world_logging = { k: '#' for k in keys }
-                print('World (last 50):', world_logging, '\n')
-                print('Rock pos:', curr_rock_pos, '\n')
-
+        # Get current move and determine offset
         move = data[ir]
         if move == '<':
             offset = -1
         else:
             offset = 1
 
-        # Calc if left/right movement is valid
-        can_move = True
-        new_rock_pos = [p for p in curr_rock_pos]
-        for pos in range(len(new_rock_pos)):
-            x, y = new_rock_pos[pos]
-            if (x + offset == -1) or (x + offset == 7) or world.get((x + offset, y), '.') == '#':
-                can_move = False
+
+        # Calc left/right movement
+        allowed = True
+        new_rock_pos = [_ for _ in rock_pos]
+        for p in range(len(new_rock_pos)):
+            x, y = new_rock_pos[p]
+            if (x+offset == -1) or (x+offset == 7) or world.get((x+offset, y), '.') == '#':
+                allowed = False
                 break
             else:
-                new_rock_pos[pos] = (x + offset, y)
+                new_rock_pos[p] = (x+offset, y)
 
-        if can_move:
-            curr_rock_pos = [p for p in new_rock_pos]
+        if allowed:
+            rock_pos = [_ for _ in new_rock_pos]
 
-        # If downwards movement is valid
-        can_move = True
-        new_rock_pos = [p for p in curr_rock_pos]
-        for pos in range(len(new_rock_pos)):
-            x, y = new_rock_pos[pos]
-            if (y - 1 == -1) or world.get((x, y - 1), '.') == '#':
-                can_move = False
+        # Calc downwards movement
+        allowed = True
+        new_rock_pos = [_ for _ in rock_pos]
+        for p in range(len(new_rock_pos)):
+            x, y = new_rock_pos[p]
+            if (y-1 == -1) or world.get((x, y-1), '.') == '#':
+                allowed = False
                 break
             else:
-                new_rock_pos[pos] = (x, y - 1)
+                new_rock_pos[p] = (x, y-1)
+        
+        if allowed:
+            rock_pos = [_ for _ in new_rock_pos]
 
-        if can_move:
-            curr_rock_pos = [p for p in new_rock_pos]
+        # Downwards movement execution
+        if allowed:
+            rock_pos = [_ for _ in new_rock_pos]
         else:
-            # End current rock
-            for pos in curr_rock_pos:
-                world.setdefault(pos, '#')
-                _, y = pos
-                if (y + 1) > curr_height:
-                    curr_height = y + 1
 
-            # Setup next rock
-            fallen_count += 1
-            curr_rock_type = (curr_rock_type + 1) % len(rock_types.keys())
-            curr_rock_pos = [p for p in rock_types[curr_rock_type]]
-            for pos in range(len(curr_rock_pos)):
-                x, y = curr_rock_pos[pos]
-                x += 2
-                y += curr_height + 3
-                curr_rock_pos[pos] = (x,y)
+            # Rock movement ceased
+            for p in rock_pos:
+                world.setdefault(p, '#')
+                if (p[1] + 1) > height:
+                    height = p[1] + 1
+
+            # Next rock and instruction
+            fallen += 1
+            rock_type = (rock_type + 1) % len(rocks.keys())
+            rock_pos = [_ for _ in rocks[rock_type]]
+            rock_pos = [(x+2,y+height+3) for x,y in rock_pos]
         
         ir = (ir + 1) % len(data)
 
-    return curr_height, cycles
+    return height
 
 world = {}
-height, _ = simulate_rockfall(data, world, 2022)
+height = simulate_rockfall(data, world, 2022)
 print('Part 1:', height)
 
 
 # Part two
-# TODO
