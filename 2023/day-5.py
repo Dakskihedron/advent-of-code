d024e4332653a2b092484260d1e7c11230026fab
Nathan Zhou <nathan.zhou@koordinates.com>
2023-12-06 16:02:00 +1300

    chore: improve day 5 solution

--- 2023/day-5.py
+++ 2023/day-5.py
@@ -44,65 +44,46 @@ def low_loc(data):
     print('Part one:', min(locations))
 
 
-def cut_interval(start, end, src_start, dest_start, range):
-    # If interval is overlaps than src interval
-    if start < src_start and end >= src_start + range:
-        return (
-            start, src_start-1,
-            dest_start, dest_start+range-1,
-            src_start + range, end
-            )
-    # If interval start is less than src start
-    elif start < src_start and end < src_start + range and end >= src_start:
-        return (
-            start, src_start-1,
-            dest_start, dest_start+range-((src_start+range-1)-end)-1
-        )
-    # If interval end is greater than src end
-    elif start >= src_start and start < src_start+range and end >= src_start+range:
-        print(True)
-        return (
-            src_start+range, end,
-            dest_start+(start-src_start), dest_start+range-1
-        )
-    # If interval within src interval
-    elif start >= src_start and end < src_start+range:
-        return (
-            dest_start+(start-src_start), dest_start+range-((src_start+range-1)-end)-1, 0, 0, 0, 0, 0, 0
-        )
-    else:
-        return (start, end)
-    
-
-def dnc(start, end, mappings, map, entry):
+def dnc_intervals(start, end, mappings, map, entry):
     if map > len(maps)-1:
         return start
     if entry > len(mappings[maps[map]])-1:
         if maps[map] == 'humidity-to-location map:':
             return start
-        return dnc(start, end, mappings, map+1, 0)
+        return dnc_intervals(start, end, mappings, map+1, 0)
     
-    src = mappings[maps[map]][entry][1]
-    dest = mappings[maps[map]][entry][0]
+    src_start = mappings[maps[map]][entry][1]
+    dest_start = mappings[maps[map]][entry][0]
     range = mappings[maps[map]][entry][2]
 
-    res = cut_interval(start, end, src, dest, range)
-
-    if len(res) == 2:
-        return dnc(start, end, mappings, map, entry+1)
-    elif len(res) == 4:
+    # If interval overlaps src interval
+    if start < src_start and end >= src_start + range:
         return min(
-            dnc(res[0], res[1], mappings, map, entry+1),
-            dnc(res[2], res[3], mappings, map+1, 0)
+            dnc_intervals(start, src_start-1, mappings, map, entry+1),
+            dnc_intervals(dest_start, dest_start+range-1, mappings, map+1, 0),
+            dnc_intervals(src_start + range, end, mappings, map, entry+1)
         )
-    elif len(res) == 6:
+    # If interval start is less than src start
+    elif start < src_start and end < src_start + range and end >= src_start:
         return min(
-            dnc(res[0], res[1], mappings, map, entry+1),
-            dnc(res[2], res[3], mappings, map+1, 0),
-            dnc(res[4], res[5], mappings, map, entry+1)
+            dnc_intervals(start, src_start-1, mappings, map, entry+1),
+            dnc_intervals(dest_start, dest_start+range-((src_start+range-1)-end)-1, mappings, map+1, 0)
+        )
+    # If interval end is greater than src end
+    elif start >= src_start and start < src_start+range and end >= src_start+range:
+        return min(
+            dnc_intervals(src_start+range, end, mappings, map, entry+1),
+            dnc_intervals(dest_start+(start-src_start), dest_start+range-1, mappings, map+1, 0)
+        )
+    # If interval within src interval
+    elif start >= src_start and end < src_start+range:
+        return dnc_intervals(
+            dest_start+(start-src_start),
+            dest_start+range-((src_start+range-1)-end)-1,
+            mappings, map+1, 0
         )
     else:
-        return dnc(res[0], res[1], mappings, map+1, 0)
+        return dnc_intervals(start, end, mappings, map, entry+1)
 
 
 # Part two
@@ -111,11 +92,9 @@ def eff_low_loc(data):
     seed_ranges = {seeds[i]: seeds[i]+seeds[i+1]-1 for i in range(0, len(seeds)-1, 2)}
     data = data[1:]
     mappings = create_mappings(data)
-
     locations = []
     for start,end in seed_ranges.items():
-        locations.append(dnc(start, end, mappings, 0, 0))
-
+        locations.append(dnc_intervals(start, end, mappings, 0, 0))
     print('Part two:', min(locations))
 
 
